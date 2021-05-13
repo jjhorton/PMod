fa6bc171327f5a15b481081475054f1e0201251e
jjhorton <jjhorton@gmail.com>
2021-05-13 20:06:53 +0100

    Inital outline main and cmakelist files

---
+++ PMod_CC1101/Software/main.c
@@ -0,0 +1,35 @@
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+// write to a register
+static void write_register(uint8_t reg, uint8_t data) {
+    uint8_t buf[2];
+    buf[0] = reg & 0x7f;  // remove read bit as this is a write
+    buf[1] = data;
+    cs_select();
+    spi_write_blocking(spi_default, buf, 2);
+    cs_deselect();
+    sleep_ms(10);
+}
+
+//read from a register
+static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
+    reg |= READ_BIT;
+    cs_select();
+    spi_write_blocking(spi_default, &reg, 1);
+    sleep_ms(10);
+    spi_read_blocking(spi_default, 0, buf, len);
+    cs_deselect();
+    sleep_ms(10);
+}
+
+
+// the main function
+int main(){
+
+	printf("Hello, Reading raw data from registers via SPI...\n");
+
+}
