b1325b41941b62af5ebe288f712d3d536308a276
jjhorton <jjhorton@gmail.com>
2021-05-27 22:42:34 +0100

    fix the lint checks

--- PMod_Neopixel/Firmware/writepixel.v
+++ PMod_Neopixel/Firmware/writepixel.v
@@ -1,5 +1,5 @@
 module writepixel(
-	input CLK,
+	input clk,
 	input value,
 	input valid,
 
@@ -10,6 +10,8 @@ module writepixel(
 	reg [31:0] counter =0;
 	reg my_value = 0;
 	reg pixel_clk = 0;
+	reg data_out = 0;
+	reg busy_out = 0;
 
 	// states for the state machine
 	parameter 	IDLE 	= 0;
@@ -24,7 +26,7 @@ module writepixel(
 	parameter 	clk_divider_count = clk_in_rate_hz/(clk_pixel_rate_hz);
 
 	//clock divider for setting the rate that the state can change
-	always @(posedge CLK) begin
+	always @(posedge clk) begin
 		if (counter > clk_divider_count)
 			counter <= counter + 1;
 		else
@@ -35,22 +37,23 @@ module writepixel(
 	// data_ready used to trigger the next bit each time
 	reg data_ready = 0;
 	//read in the value for the output
-	always @(posedge CLK) begin
-		if (valid = 1)
+	always @(posedge clk) begin
+		if (valid == 1)
 		begin
 			my_value = value;
-			data_ready = 1'b1;
+			data_ready <= 1'b1;
 		end
+
 		if (busy)
-			data_ready = 1'b0
+			data_ready <= 1'b0;
 	end
 
 	//output the ready state, based on the state register
-	always @(posedge CLK) begin
-		if (state = IDLE)
-			busy <= 1'b0;
+	always @(posedge clk) begin
+		if (state == IDLE)
+			busy_out <= 1'b0;
 		else
-			busy <= 1'b1;
+			busy_out <= 1'b1;
 	end
 
 	//state machine for sending each value
@@ -58,33 +61,38 @@ module writepixel(
 		case(state)
 			IDLE:
 				begin
-					if (data_ready = 1)
+					if (data_ready == 1)
 						state <= STATE1;
 				end
 			STATE1:
 				begin
 					//first part, always high
-					d_out <= 1'b1;
+					data_out <= 1'b1;
 					state <= STATE2;
 				end
 			STATE2:
 				begin
 					//second part, always high
-					d_out <= 1'b1;
+					data_out <= 1'b1;
 					state <= STATE3;
 				end
 			STATE3:
 				begin
 					// high for a 1, low for 0
-					d_out <= my_value;
+					data_out <= my_value;
 					state <= STATE4;
 				end
 			STATE4:
 				begin
 					// alway low
-					d_out <= 1'b0;
+					data_out <= 1'b0;
 					state <= IDLE;
 				end
+			endcase
 
-	end
+		end
+
+		assign d_out = data_out;
+		assign busy = busy_out;
+
 endmodule
