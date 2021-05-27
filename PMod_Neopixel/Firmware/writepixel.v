3632456e59ce6d98323304788a91dc0a4a474f0a
jjhorton <jjhorton@gmail.com>
2021-05-27 22:00:31 +0100

    Add outline of the state machine

--- PMod_Neopixel/Firmware/writepixel.v
+++ PMod_Neopixel/Firmware/writepixel.v
@@ -8,13 +8,18 @@ module writepixel(
 
 	reg [2:0] state = 0;
 	reg [31:0] counter =0;
+	reg my_value = 0;
 	reg pixel_clk = 0
 
+	// states for the state machine 
+
+
+	// clock rate for calculating the clock divider
 	parameter 	clk_in_rate_hz = 12_000_000;
 	parameter		clk_pixel_rate_hz = 80_000;
 	parameter 	clk_divider_count = clk_in_rate_hz/(clk_pixel_rate_hz);
 
-	//clock deivder for setting the rate that the state can change
+	//clock divider for setting the rate that the state can change
 	always @(posedge CLK) begin
 		if (counter > clk_divider_count)
 			counter <= counter + 1;
@@ -23,8 +28,53 @@ module writepixel(
 			pixel_clk <= ~pixel_clk;
 	end
 
-	//state machine for sending each value
+	//read in the value for the output
 	always @(posedge CLK) begin
+		if (valid = 1)
+		begin
+			my_value = value;
+		end
+	end
+
+	//output the ready state, based on the state veriable
+	always @(posedge CLK) begin
+		if (state = IDLE)
+			ready <= 1'b1;
+		else
+			ready <= 1'b0;
+	end
+
+	//state machine for sending each value
+	always @(posedge pixel_clk) begin
+		case(state)
+			IDLE:
+				begin
+
+				end
+			STATE1:
+				begin
+				//first part, always high
+				d_out <= 1'b1;
+				state <= STATE2;
+				end
+			STATE2:
+				begin
+				//second part, always high
+				d_out <= 1'b1;
+				state <= STATE3;
+				end
+			STATE3:
+				begin
+				// high for a 1, low for 0
+				d_out <= my_value;
+				state <= STATE4;
+				end
+			STATE4:
+				begin
+				// alway low
+				d_out <= 1'b0;
+				state <= IDLE;
+				end
 
 	end
 
