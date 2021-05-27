228a5ee19d56e38ef8e01c776616c907ded4fec1
jjhorton <jjhorton@gmail.com>
2021-05-27 22:43:10 +0100

    inital verilator check program

---
+++ PMod_Neopixel/Firmware/writepixel.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Vwritepixel.h"
+#include "verilated_vcd_c.h"
+#include "verilated.h"
+
+void tick(int tickcount, Vwritepixel *tb, VerilatedVcdC* tfp){
+	tb->eval();
+
+	if(tfp)
+		tfp->dump(tickcount * 10-2);
+
+	tb->clk = 1;
+	tb->eval();
+	if(tfp)
+		tfp->dump(tickcount * 10);
+
+	tb->clk = 0;
+	tb->eval();
+	if (tfp) {
+		tfp->dump(tickcount * 10 + 5);
+		tfp->flush();
+	}
+}
+
+int main(int argc, char **argv){
+	Verilated::commandArgs(argc, argv);
+
+	// Instantiate the design
+	Vwritepixel *tb = new Vwritepixel;
+
+	unsigned tickcount = 0;
+
+	//generate a trace
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	tb->trace(tfp, 99);
+	tfp->open("trace.vcd");
+
+	//copy the data
+	tb->value = 0;
+	tb->valid = 1;
+
+	for(int i=0; i<100; i++){
+
+		tick(++tickcount, tb, tfp);
+		tb->valid = 0;
+
+	}
+}
