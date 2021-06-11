d3e590ccd979868d21712ad08412fa028278a27d
jjhorton <jjhorton@gmail.com>
2021-06-11 22:44:01 +0100

    Add the test bench command

---
+++ PMod_Neopixel/Firmware/writepixel_tb.sh
@@ -0,0 +1,6 @@
+export VINC=/opt/homebrew/Cellar/verilator/4.108/share/verilator/include
+verilator -Wall --trace -Gclk_in_rate_hz=12000000 -cc writepixel.v
+make -C obj_dir -f Vwritepixel.mk
+g++ -I${VINC} -I obj_dir ${VINC}/verilated.cpp ${VINC}/verilated_vcd_c.cpp writepixel.cpp obj_dir/Vwritepixel__ALL.a -o writepixel
+clang++ -std=gnu++14 -I${VINC} -I obj_dir ${VINC}/verilated.cpp ${VINC}/verilated_vcd_c.cpp writepixel.cpp obj_dir/Vwritepixel__ALL.a -o writepixel
+./writepixel 
