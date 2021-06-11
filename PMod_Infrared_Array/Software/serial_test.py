6e16393768b6aceb0e94ea00bf1fa701e19cac89
jjhorton <jjhorton@gmail.com>
2021-06-11 16:35:54 +0100

    Set the colour map, and flip the image correctly

--- PMod_Infrared_Array/Software/serial_test.py
+++ PMod_Infrared_Array/Software/serial_test.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-import csv
 
 import serial
 
@@ -13,7 +11,7 @@ my_counter = 0
 
 fg = plt.figure()
 ax = fg.gca()
-h = ax.imshow(my_result, vmin=10, vmax=40)
+h = ax.imshow(my_result, vmin=0, vmax=40,cmap=plt.get_cmap('inferno'))
 plt.colorbar(h)
 
 while(1):
@@ -29,7 +27,7 @@ while(1):
         my_counter = my_counter + 1
 
         if my_counter > 7:
-            h.set_data(my_result)
+            h.set_data(np.fliplr(np.flip(my_result)))
             plt.draw(), plt.pause(1e-3)
             print(my_result)
             print(my_result.dtype)
