598997fd84affb52d55f53a7d4b9796c7ab69601
jjhorton <jjhorton@gmail.com>
2021-06-14 16:05:57 +0100

    Add initial attempt at FPGA code for icebreaker board

---
+++ PMod_LED/Firmware/blinky.v
@@ -0,0 +1,28 @@
+`default_nettype none
+
+module blinky(CLK, LED);
+	input 	wire 					CLK;
+	output 	wire 	[0:7]		LED;
+
+	parameter 	WIDTH=27;
+
+	reg 	[WIDTH-1:0]		counter;
+	reg 	[0:7]					display;
+
+	initial counter = 0;
+	initial display = 8'b00000001;
+
+	always	@(posedge CLK)
+	begin
+		counter <= counter +1'b1;
+
+		if (counter[WIDTH-1] == 1)
+		begin
+			// When the counter reaches top value shift the LEd accross one
+			counter = 0;
+			display = display << 1;
+		end
+	end
+
+	assign LED = display;
+endmodule
