34b4cf343214a76b0762c5fade77535359704bcc
jjhorton <jjhorton@gmail.com>
2021-06-14 21:47:26 +0100

    fix errors highlighted by verilator check

--- PMod_LED/Firmware/blinky.v
+++ PMod_LED/Firmware/blinky.v
@@ -2,12 +2,12 @@
 
 module blinky(CLK, LED);
 	input 	wire 					CLK;
-	output 	wire 	[0:7]		LED;
+	output 	wire 	[7:0]		LED;
 
 	parameter 	WIDTH=27;
 
 	reg 	[WIDTH-1:0]		counter;
-	reg 	[0:7]					display;
+	reg 	[7:0]					display;
 
 	initial counter = 0;
 	initial display = 8'b00000001;
@@ -19,7 +19,7 @@ module blinky(CLK, LED);
 		if (counter[WIDTH-1] == 1)
 		begin
 			// When the counter reaches top value shift the LEd accross one
-			counter = 0;
+			counter <= 0;
 			display = display << 1;
 		end
 	end
