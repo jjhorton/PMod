587766e17eaa58b82e1b9a986c6030ffc0c172d2
jjhorton <jjhorton@gmail.com>
2021-06-24 22:36:25 +0100

    Update the Pins and correct the case of moving from 8 LED to 1st LED

--- PMod_LED/Firmware/blinky.v
+++ PMod_LED/Firmware/blinky.v
@@ -1,18 +1,18 @@
 `default_nettype none
 
 module blinky(CLK, LED);
-	input 	wire 					CLK;
-	output 	wire 	[7:0]		LED;
+	input 	wire 			CLK;
+	output 	wire 	[7:0]	LED;
 
-	parameter 	WIDTH=27;
+	parameter 	WIDTH=25;
 
 	reg 	[WIDTH-1:0]		counter;
-	reg 	[7:0]					display;
+	reg 	[7:0]			display;
 
 	initial counter = 0;
 	initial display = 8'b00000001;
 
-	always	@(posedge CLK)
+	always @(posedge CLK)
 	begin
 		counter <= counter +1'b1;
 
@@ -20,7 +20,10 @@ module blinky(CLK, LED);
 		begin
 			// When the counter reaches top value shift the LEd accross one
 			counter <= 0;
-			display = display << 1;
+			if (display== 8'b10000000)
+				display <= 8'b00000001;
+			else
+				display <= display << 1;
 		end
 	end
 
