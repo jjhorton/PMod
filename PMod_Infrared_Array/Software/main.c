399b419727d459ff8527b7aac073ad8064eb0133
jjhorton <jjhorton@gmail.com>
2021-06-24 21:48:59 +0100

    Update the comments and the copyright statement, remove unused code

--- PMod_Infrared_Array/Software/main.c
+++ PMod_Infrared_Array/Software/main.c
@@ -1,45 +1,24 @@
 /**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ * Copyright (c) 2021 James Horton
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ *	Simple example for working with the AMG 8834 sesnor
+ *
  */
 
-// Sweep through all 7-bit I2C addresses, to see if any slaves are present on
-// the I2C bus. Print out a table that looks like this:
-//
-// I2C Bus Scan
-//   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
-// 0
-// 1       @
-// 2
-// 3             @
-// 4
-// 5
-// 6
-// 7
-//
-// E.g. if slave addresses 0x12 and 0x34 were acknowledged.
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
-// I2C reserves some addresses for special purposes. We exclude these from the scan.
-// These are any addresses of the form 000 0xxx or 111 1xxx
-bool reserved_addr(uint8_t addr) {
-    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
-}
 
 int main() {
     // Enable UART so we can print status output
     stdio_init_all();
     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
     i2c_init(i2c1, 1000000);
-    //gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    //gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    //gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    //gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+		//Setup the I2C pins
 		gpio_set_function(2, GPIO_FUNC_I2C);
     gpio_set_function(3, GPIO_FUNC_I2C);
     gpio_pull_up(2);
@@ -48,6 +27,7 @@ int main() {
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(2, 3, GPIO_FUNC_I2C));
 
+		//setup the pins for the flashing status LED
 		int8_t on_off = 0;
 		gpio_init(20);
 		gpio_set_dir(20, GPIO_OUT);
