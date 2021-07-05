74bf91588c91b7086c5eaa2785e5abaf5e93f8a5
jjhorton <jjhorton@gmail.com>
2021-07-05 23:20:27 +0100

    Add a second write out test after 512 clock cyles

--- PMod_Neopixel/Firmware/writepixel.cpp
+++ PMod_Neopixel/Firmware/writepixel.cpp
@@ -44,6 +44,10 @@ int main(int argc, char **argv){
 	tb->valid = 1;
 
 	for(int i=0; i<1000; i++){
+		if(i==512) {
+			tb->pixel_b =  int8_t(255);
+			tb->valid = 1;
+		}
 
 		tick(++tickcount, tb, tfp);
 		tb->valid = 0;
