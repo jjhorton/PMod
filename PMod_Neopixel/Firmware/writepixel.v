95898efbab1af27c4f227f72057b01703bca4fa3
jjhorton <jjhorton@gmail.com>
2021-07-06 23:32:43 +0100

    fix error in the clock divider

--- PMod_Neopixel/Firmware/writepixel.v
+++ PMod_Neopixel/Firmware/writepixel.v
@@ -22,23 +22,25 @@ module writepixel(
 
 	// states for the state machine
 	parameter 	IDLE 	= 0;
-	parameter 	STATE1= 1;
-	parameter 	STATE2= 2;
-	parameter 	STATE3= 3;
-	parameter 	STATE4= 4;
+	parameter 	STATE1	= 1;
+	parameter 	STATE2	= 2;
+	parameter 	STATE3	= 3;
+	parameter 	STATE4	= 4;
 
 	// clock rate for calculating the clock divider
 	parameter 	clk_in_rate_hz = 12_000_000;
-	parameter		clk_pixel_rate_hz = 100_000;
+	parameter		clk_pixel_rate_hz = 2_500_000;
 	parameter 	clk_divider_count = clk_in_rate_hz/(clk_pixel_rate_hz);
 
 	//clock divider for setting the rate that the state can change
 	always @(posedge clk) begin
-		if (counter > clk_divider_count)
-			counter <= counter + 1;
+		if (counter < clk_divider_count)
+			counter <= counter + 1'b1;
 		else
+			begin
 			counter <= 0;
 			pixel_clk <= ~pixel_clk;
+			end
 	end
 
 	// data_ready used to trigger the next bit each time
@@ -59,7 +61,8 @@ module writepixel(
 
 	//output the ready state, based on the state register
 	always @(posedge clk) begin
-		if (state == IDLE)
+		if ((state == IDLE)||((state == STATE4)&(count_bit == 0)))
+		//if (state == IDLE)
 			busy_out <= 1'b0;
 		else
 			busy_out <= 1'b1;
@@ -78,7 +81,7 @@ module writepixel(
 				begin
 					//first part, always high
 					data_out <= 1'b1;
-					state <= STATE2;
+					state <= STATE3;
 					count_bit <= count_bit - 1;
 				end
 			STATE2:
