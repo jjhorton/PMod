1384d892fb2327b2b917ecf539573d327393bb04
jjhorton <jjhorton@gmail.com>
2021-07-08 22:36:02 +0100

    Add the single pixel serial control python script

---
+++ PMod_Neopixel/Firmware/pixelserial.py
@@ -0,0 +1,37 @@
+import numpy as np
+import time
+
+import serial
+
+def segments(data, size=1):
+    for a in range(0, len(data), size):
+        yield data[a:a + size]
+
+ser = serial.Serial('/dev/tty.usbserial-ib2u9O9Z1',19200)
+
+print(ser.name)
+print(ser.baudrate)
+
+ser.write(b'z')
+time.sleep(1)
+ser.write(b'A')
+time.sleep(1)
+
+x = bytes(bytearray(range(256)))
+
+
+for count in segments(x):
+	print(count)
+	ser.write(count)
+	time.sleep(0.1)
+
+ser.close()
+
+
+#ser = serial.Serial('/dev/tty.usbserial-ib2u9O9Z1',115200,timeout=1)
+
+#ser.write(0b10000000)
+#time.sleep(1)
+#ser.write(0b00000000)
+
+#ser.close()
