74d35bc096769f36a8525f10922840b2c050aa93
jjhorton <jjhorton@gmail.com>
2021-08-28 22:08:02 +0100

    Outline of the software for the 7 seg display

---
+++ PMod_7Segment/Software/main.c
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pico/stdlib.h"
+
+int set_display(){
+	
+}
+
+
+int main() {
+
+
+	while(1){
+
+		sleep_ms(1000);
+	}
+
+}
