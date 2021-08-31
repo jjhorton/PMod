720302c541d4ae615357046ea12eb50fb592d2ff
jjhorton <jjhorton@gmail.com>
2021-08-31 20:20:03 +0100

    Update to flash all the leds in sequence arround the board

--- PMod_LED/Software/main.c
+++ PMod_LED/Software/main.c
@@ -9,7 +9,7 @@
 #else
 	//if plugged into pmod0
 	//const uint OUTPUT_PINS[] = {21,19,17,3,20,18,16,2};
-	const uint OUTPUT_PINS[] = {21,20,19,18,17,16,3,2};
+	const uint OUTPUT_PINS[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
 #endif
 
 int setup_output(){
