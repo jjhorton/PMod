936529e2752451e4e527d990405b704cec32b63f
jjhorton <jjhorton@gmail.com>
2021-09-29 19:57:16 +0100

    Make each of the PMOD LED outputs indipendant and cycle at the same time

--- PMod_LED/Software/main.c
+++ PMod_LED/Software/main.c
@@ -9,14 +9,24 @@
 #else
 	//if plugged into pmod0
 	//const uint OUTPUT_PINS[] = {21,19,17,3,20,18,16,2};
-	const uint OUTPUT_PINS[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+	const uint OUTPUT_PINS_1[] = {0,1,2,3,4,5,6,7};
+	const uint OUTPUT_PINS_2[] = {8,9,10,11,12,13,14,15};
+	const uint OUTPUT_PINS_3[] = {16,17,18,19,20,21,22,23};
 #endif
 
 int setup_output(){
 	for(int x = 0; x<8; x++) {
-			gpio_init(OUTPUT_PINS[x]);
-			gpio_set_dir(OUTPUT_PINS[x], GPIO_OUT);
-			gpio_put(OUTPUT_PINS[x],0);
+			gpio_init(OUTPUT_PINS_1[x]);
+			gpio_set_dir(OUTPUT_PINS_1[x], GPIO_OUT);
+			gpio_put(OUTPUT_PINS_1[x],0);
+
+			gpio_init(OUTPUT_PINS_2[x]);
+			gpio_set_dir(OUTPUT_PINS_2[x], GPIO_OUT);
+			gpio_put(OUTPUT_PINS_2[x],0);
+
+			gpio_init(OUTPUT_PINS_3[x]);
+			gpio_set_dir(OUTPUT_PINS_3[x], GPIO_OUT);
+			gpio_put(OUTPUT_PINS_3[x],0);
 	}
 }
 
@@ -31,7 +41,9 @@ int main() {
 
 		for(int y = 0; y<8; y++){
 			for(int x = 0; x<8; x++) {
-				gpio_put(OUTPUT_PINS[x], (my_value >> x) & 1U);
+				gpio_put(OUTPUT_PINS_1[x], (my_value >> x) & 1U);
+				gpio_put(OUTPUT_PINS_2[x], (my_value >> x) & 1U);
+				gpio_put(OUTPUT_PINS_3[x], (my_value >> x) & 1U);
 			}
 			my_value = my_value << 1;
 			sleep_ms(100);
