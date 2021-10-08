03f4e0cd8bed209b11f5791685e396dd5fc4aa80
jjhorton <jjhorton@gmail.com>
2021-10-08 22:14:39 +0100

    Create a static text display on the display

--- PMod_LEDArray/Software/main.cpp
+++ PMod_LEDArray/Software/main.cpp
@@ -12,26 +12,39 @@
 
 int main() {
 
-	//LedDisp Display1A(16,18);
-	//LedDisp Display1B(8,10);
+
 	MyLedDisp Display2(0,2);
-
-	uint8_t display[16];
-
-
-	//Display1A.enable();
-	//Display1B.enable();
 	Display2.enable();
+
+	uint8_t display_buff[16];
+
+	for(uint8_t pos = 0; pos<16; pos++){
+		display_buff[pos] = 0b00000000;
+	}
+
+	display_buff[0] = 0b10000010;
+	display_buff[1] = 0b10000001;
+	display_buff[2] = 0b11111110;
+	display_buff[3] = 0b10000000;
+	display_buff[4] = 0b00000000;
+	display_buff[5] = 0b10000010;
+	display_buff[6] = 0b10000001;
+	display_buff[7] = 0b11111110;
+	display_buff[8] = 0b10000000;
+	display_buff[9] = 0b00000000;
+	display_buff[10] = 0b11111111;
+	display_buff[11] = 0b00001000;
+	display_buff[12] = 0b00001000;
+	display_buff[13] = 0b11111111;
 
 	while(1){
 
-		for(int value=0; value<16; ++value){
-			//for(int i = 0; i<16; ++i){
-				Display2.setdigit(value, rand() % 10, true);
-			//}
+		for(uint8_t pos = 0; pos<16; pos++){
+			//display_buff[pos] =
+			//display_buff[pos] <<= 1;
 		}
-		//Display1A.setValue2(double(987.6), double(123.4),1);
-		//Display1B.setValue(double(1234.5678), 4);
+
+	  Display2.setDisplay(display_buff);
 
 		sleep_ms(1000);
 
