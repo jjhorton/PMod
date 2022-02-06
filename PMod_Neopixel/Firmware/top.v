98cfcbc23cf1a1e442a9671356274860fbeccb3e
jjhorton <jjhorton@gmail.com>
2022-02-06 20:56:37 +0000

    Add serial RGB writing in serial and correct error in top counter

--- PMod_Neopixel/Firmware/top.v
+++ PMod_Neopixel/Firmware/top.v
@@ -107,7 +107,7 @@ module top (CLK,o_PMOD1A, o_PMOD1B, RX);
 				else
 					begin
 						rx_count <= 4'b0000;
-						if (led_count < 10)
+						if (led_count < 9)
 							led_count <= led_count + 1'b1;
 						else
 							led_count <= 0;
