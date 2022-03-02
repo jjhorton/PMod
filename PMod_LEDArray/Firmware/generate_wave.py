949f4fd346c16c3c47e79a05f897fe38a8afcd0e
jjhorton <jjhorton@gmail.com>
2022-03-02 23:06:48 +0000

    correct the path to waveforms

--- PMod_LEDArray/Firmware/generate_wave.py
+++ PMod_LEDArray/Firmware/generate_wave.py
@@ -3,8 +3,8 @@ sys.path.insert(0,'../../../vcd2json')
 
 from vcd2json import WaveExtractor
 
-path_list = ['TOP/writepixels/d_clk',
-             'TOP/writepixels/d_out']
+path_list = ['TOP/top/writepixels/d_clk',
+             'TOP/top/writepixels/d_out']
 
 extractor = WaveExtractor('trace.vcd', 'trace.json',path_list)
 extractor.wave_format('TOP/blinky/display', 'b')
