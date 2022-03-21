df6084782356bc0d7da03f8e041293250a7e3e56
jjhorton <jjhorton@gmail.com>
2022-03-21 20:52:49 +0000

    move elemets into pixel control

--- PMod_LEDArray/Firmware/top.v
+++ PMod_LEDArray/Firmware/top.v
@@ -7,7 +7,6 @@ module top (CLK, o_PMOD1A, o_PMOD1B);
 
 
     reg [7:0] tx_value [0:15];
-    reg [1:0] system_state = 2'b00;
 	reg [7:0] pmod1a;
 	reg [7:0] pmod1b = 8'b00000000;
     reg valid;
@@ -49,83 +48,9 @@ module top (CLK, o_PMOD1A, o_PMOD1B);
                 clk_counter <= 0;
                 pps <= 1;
             end
-    end
+    end   
 
-    // state Machine for setting display
-    // State Machine States
-    parameter 	IDLE 	    = 0;
-	parameter 	INITDISPLAY	= 1;
-    parameter 	PAUSE	    = 2;
-    parameter 	SETDISPLAY	= 3;
-
-    reg [3:0] byte_count;
-    reg [31:0] pause_counter;
-    reg [7:0] pos = 8'b00000000;
-
-    always @(posedge CLK) begin
-		case(system_state)
-			IDLE:
-            begin
-                if(pps == 1)
-                    system_state <= INITDISPLAY;
-
-                valid <= 0;
-            end
-
-            INITDISPLAY:
-            begin
-                // if the display is not busy set the input
-                if((busy == 0) & (valid == 0))
-                begin
-                    value <= 8'b10001001;
-                    valid <= 1;
-                    system_state <= PAUSE;
-                    byte_count <= 0;
-                    pos <= 8'b11111111;
-                end
-                else
-                    valid <= 0;
-
-                pause_counter <= 0;
-            end
-
-            PAUSE:
-            begin 
-                valid <= 0;
-                pause_counter <= pause_counter + 1;
-                if(pause_counter < (clk_in_rate_hz/1000) )
-                    system_state <= PAUSE;
-                else
-                    system_state <= SETDISPLAY;
-            end
-
-            SETDISPLAY:
-            begin 
-                // if the display is not busy set the input
-                if((busy == 0) & (valid == 0))
-                begin
-                    /* verilator lint_off WIDTH */
-                    pos <= 8'b11000000 + byte_count;
-                    /* verilator lint_on WIDTH */
-
-                    value <= tx_value[byte_count];
-                    valid <= 1;
-
-                    if(byte_count<15)
-                    begin
-                        system_state <= SETDISPLAY;
-                        byte_count <= byte_count + 1;
-                    end
-                    else
-                        system_state <= IDLE;
-                end
-                else
-                    valid <= 0;
-            end
-        endcase
-    end
-
-    writepixels writepixels(CLK ,valid, pos, value, pmod1a[6], pmod1a[7], busy);
+    writepixels writepixels(CLK , pmod1a[6], pmod1a[7], pps);
 
     assign pmod1a[5:0] = 6'b000000;
 
