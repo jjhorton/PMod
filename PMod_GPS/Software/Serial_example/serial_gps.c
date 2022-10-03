f3ee9fe869bef80335524b5b0ddcbf02c4e78f8a
jjhorton <jjhorton@gmail.com>
2022-10-03 19:52:57 +0100

    outlines to start writing the rp2040 code

---
+++ PMod_GPS/Software/Serial_example/serial_gps.c
@@ -0,0 +1,15 @@
+
+// Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Pico Libraries
+#include "pico/stdlib.h"
+#include "pico/stdio_usb.h"
+
+int main(){
+
+    stdio_init_all();
+
+
+}
