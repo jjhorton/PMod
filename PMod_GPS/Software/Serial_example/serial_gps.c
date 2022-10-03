b0525d245096347aa91d1107fd3dd3ba5008a914
jjhorton <jjhorton@gmail.com>
2022-10-03 20:37:29 +0100

    start defining a simple forwarding solution

--- PMod_GPS/Software/Serial_example/serial_gps.c
+++ PMod_GPS/Software/Serial_example/serial_gps.c
@@ -1,15 +1,31 @@
-
-// Standard Libraries
 #include <stdio.h>
-#include <stdlib.h>
-
-// Pico Libraries
 #include "pico/stdlib.h"
 #include "pico/stdio_usb.h"
+#include "hardware/uart.h"
 
-int main(){
+//uart parameters
+#define UART_ID uart1
+#define BAUD_RATE 9600
+#define UART_TX_PIN 8
+#define UART_RX_PIN 9
 
+
+int main() {
+
+    // see: https://github.com/raspberrypi/pico-examples/blob/master/uart/lcd_uart/lcd_uart.c
     stdio_init_all();
+    uart_init(UART_ID, BAUD_RATE);
+    uart_set_translate_crlf(UART_ID, false);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
+    sleep_ms(2000);
+    printf("hello world\n");
+
+    while (1) {
+        // send any chars from stdio straight to the host
+        char c = uart_getc(UART_ID);
+        if (c < 128) printf(c);
+    }
 
 }
